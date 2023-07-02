a59347fa7bfb3f57068495bf3a995a6309b2edb7
Javier Aponte <31341776+javier-aponte@users.noreply.github.com>
2023-07-01 19:39:23 -0500

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "login"
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
